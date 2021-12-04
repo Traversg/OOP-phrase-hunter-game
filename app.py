d14ab8d313a71225dd28f60ee8a6e140a88a191b
Traversg <57022409+Traversg@users.noreply.github.com>
2021-12-03 19:14:28 -0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,15 @@
+# Import your Game class
+import game
+import phrase
+
+# Create your Dunder Main statement.
+if __name__ == "__main__":
+    game = Game()
+    phrase = Phrase()
+   
+# Inside Dunder Main:
+## Create an instance of your Game class
+## Start your game by calling the instance method that starts the game loop
+
+print(phrase)
+print(game)
