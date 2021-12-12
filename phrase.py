11179415fecc1b20af035346096865034201187a
Traversg <57022409+Traversg@users.noreply.github.com>
2021-12-11 18:26:08 -0600

    Update phrase.py

--- phrase.py
+++ phrase.py
@@ -1,3 +1,26 @@
+# Create your Phrase class logic here.
+
 class Phrase:
     
-    pass
+    def __init__(self, phrase):
+        self.phrase = phrase.lower()
+        
+    def display(self, guesses):
+        for letter in self.phrase:
+            if letter in guesses:
+                print(f'{letter}', end=' ')
+            else:
+                print('_ ', end=' ')
+                
+    def check_guess(self, guess):
+        if guess in self.phrase:
+            return True
+        else:
+            return False
+        
+    def check_complete(self, guesses):
+        for letter in self.phrase:
+            if not letter in guesses:
+                return False
+            else:
+                return True
