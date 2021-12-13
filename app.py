42fe29391b8ab748f67f89409928e8e553e5a729
Traversg <57022409+Traversg@users.noreply.github.com>
2021-12-12 22:56:40 -0600

    Update app.py

--- app.py
+++ app.py
@@ -4,11 +4,9 @@ from game import Game
 # Create your Dunder Main statement.
 if __name__ == "__main__":
     
-    
-    game = Game()
-    game.start()
-    
-    
 # Inside Dunder Main:
 ## Create an instance of your Game class
 ## Start your game by calling the instance method that starts the game loop
+
+game = Game()
+game.start()
