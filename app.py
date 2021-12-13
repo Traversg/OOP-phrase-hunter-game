1c1de33039dff2b8bc53f473e557941db1458fbe
Traversg <57022409+Traversg@users.noreply.github.com>
2021-12-13 11:09:24 -0600

    Update app.py

--- app.py
+++ app.py
@@ -8,5 +8,5 @@ if __name__ == "__main__":
 ## Create an instance of your Game class
 ## Start your game by calling the instance method that starts the game loop
 
-game = Game()
-game.start()
+    game = Game()
+    game.start()
